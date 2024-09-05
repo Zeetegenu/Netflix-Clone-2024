999e26bf5908e78b48704ad90229451d8c0ae84a
Zeetegenu <zebibtegenu@outlook.com>
2024-09-04 20:08:16 -0400

    first-commit

---
+++ Netflx-Clone-2024/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+      <p className="read-the-docs">
+        {/* Click on the Vite and React logos to learn more */}
+      </p>
+    </>
+  )
+}
+
+export default App
